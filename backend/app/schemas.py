4748ae99e5cdb3e608e0c683061702ffb114ba19
usamahuzi <usamahahussain@gmail.com>
2026-07-03 17:43:02 +0100

    Add Optioanl syntax in schemas.py to overcome python3.9 issue on VM

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,15 +1,16 @@
 from pydantic import BaseModel
+from typing import Optional
 
 ## Expected input model for category update
 class CategoryUpdate(BaseModel):
     category_name: str
-    budget: float | None = None
+    budget: Optional[float] = None
 
 ## Response model, field names matching ORM model to allow model_config to link the same attributes
 class CategoryResponse(BaseModel):
     category_id: int
     category_name: str
-    budget: float | None = None
+    budget: Optional[float] = None
 
     model_config = {"from_attributes": True}
 
